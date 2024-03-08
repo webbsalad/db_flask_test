56b5975f6d7c5a98442339fffa821bc1a04716cd
webbsalad <webbbsalad@gmail.com>
2024-03-08 22:16:15 +0300

    aav

--- app.py
+++ app.py
@@ -1,65 +1,89 @@
 from flask import Flask, render_template, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+import psycopg2.extras
+import os
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///base.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(25), nullable=False)
-    mail = db.Column(db.String(50), nullable=False)
-    rating = db.Column(db.Float, default=0)
+def get_db_connection():
+    dbname = os.environ.get('DBNAME')
+    user = os.environ.get('USER')
+    password = os.environ.get('PASSWORD')
+    host = os.environ.get('HOST')
+    port = os.environ.get('PORT')
 
-    def __repr__(self):
-        return f"{self.id, self.name, self.mail, self.rating}"
+    conn = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    return conn
 
+# Create the 'item' table
 def create_tables():
-    with app.app_context():
-        db.create_all()
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS item (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(25) NOT NULL,
+            mail VARCHAR(50) NOT NULL,
+            rating FLOAT DEFAULT 0
+        )
+    """)
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 #############################################################################
 
 @app.route('/')
 def index():
-    items = Item.query.order_by(Item.id).all()
+    conn = get_db_connection()
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cursor.execute("SELECT * FROM item ORDER BY id")
+    items = cursor.fetchall()
+    cursor.close()
+    conn.close()
     return render_template("index.html", data=items)
-
 
 @app.route('/update', methods=['POST'])
 def update():
     data = request.json
+    conn = get_db_connection()
+    cursor = conn.cursor()
     for row in data:
-        item = Item.query.get(row['id'])
+        cursor.execute("SELECT * FROM item WHERE id = %s", (row['id'],))
+        item = cursor.fetchone()
         if item:
-            item.name = row['name']
-            item.mail = row['mail']
-            item.rating = row['rating']
-            db.session.commit()
+            cursor.execute("""
+                UPDATE item SET name = %s, mail = %s, rating = %s WHERE id = %s
+            """, (row['name'], row['mail'], row['rating'], row['id']))
         else:
-            new_item = Item(
-                id=row['id'],
-                name=row['name'],
-                mail=row['mail'],
-                rating=row['rating']
-            )
-            db.session.add(new_item)
-            db.session.commit()
+            cursor.execute("""
+                INSERT INTO item (id, name, mail, rating) VALUES (%s, %s, %s, %s)
+            """, (row['id'], row['name'], row['mail'], row['rating']))
+    conn.commit()
+    cursor.close()
+    conn.close()
     return 'Success'
-
 
 @app.route('/delete/<int:item_id>', methods=['DELETE'])
 def delete(item_id):
-    item = Item.query.get(item_id)
-    if item:
-        db.session.delete(item)
-        db.session.commit()
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM item WHERE id = %s", (item_id,))
+    deleted = cursor.rowcount
+    conn.commit()
+    cursor.close()
+    conn.close()
+    if deleted:
         return 'Success', 200
     else:
         return 'Not Found', 404
-
 
 if __name__ == '__main__':
     create_tables()
